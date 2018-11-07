89dffb85a937a0ef8c3fdcf3caf2c5d0360820eb
Iker Sandí Trejo <ikersanditrejo@gmail.com>
2018-11-07 11:38:07 -0600

    Fix timeout in travis

--- build.sh
+++ build.sh
@@ -91,6 +91,7 @@ error () {
 install_repo && \
 figlet "Red Wolf Recovery" && \
 echo "Syncing RedWolfRecovery Sources (rw_n)" && \
+tar xvf repo.tar && \
 repo init --depth=1 -u git://github.com/RedWolfRecovery/rw_manifest.git -b rw-n && \
 mkdir .repo/local_manifests/ && cp redwolf.xml .repo/local_manifests/ && \
 repo_sync && \
